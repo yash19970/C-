5eb0ba7ae12d39a465ce28d7e2774ace0c6bbfff
yash19970 <yash_19@outlook.com>
2016-09-20 21:52:34 +0530

    crazy computer round 372 div 2 added

---
+++ crazycomputer.cpp
@@ -0,0 +1,27 @@
+/*codeforces round #372 Crazycomputer*/
+#include<iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+	long long int n,c,z =0,latest;
+	long long int m =0;
+	cin>>n>>c; 
+	long int a[n];
+	for(int i=0;i<n;i++){
+		cin>>a[i]; 
+	}
+	for(int i=0;i<n;i++){
+		if(i+1<n){
+			if(a[i]+c >=a[i+1]){
+			m = m+1;
+		}
+		else{
+				m =0;
+				z++;
+			}
+		}	
+ 	}
+	m++;			
+	cout<<m<<endl;
+	return 0;
+}
